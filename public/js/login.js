d33319fc54b9d12d205d02014652a1280e3bde30
Jess Skelton <jessskelton888@gmail.com>
2024-01-31 13:44:48 -0600

    tweaked signup.handlebars for Password label, completed login.handlebars, & completed functionality on public/login.js --- error on line 10, fix later

---
+++ public/js/login.js
@@ -0,0 +1,26 @@
+// Login functionality
+const formHandler = async (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('#name').value.trim();
+    const password = document.querySelector('#password').value.trim();
+
+    if (name && password) {
+        const response = await fetch('/api/users/login', {
+            method: POST, // ERROR HERE ---------------------------------
+            body: JSON.stringify({ name, password }),
+            headers: { 'Content-type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Failed to log in, Please try again.');
+        };
+    };
+};
+
+// Event Listener for 'Login' button
+document
+    .querySelector('#login')
+    .addEventListener('submit', formHandler);
